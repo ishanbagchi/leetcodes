23d53417bd1b6ac64de63e3f5ba664f1f192d880
Ishan Bagchi <ishanbagchi4@gmail.com>
2022-06-14 17:47:25 +0530

    Time: 15 ms (42.64%), Space: 47.5 MB (13.71%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -7,9 +7,10 @@ class Solution {
         int len1 = word1.length();
         int len2 = word2.length();
         int[][] arr = new int[len1 + 1][len2 + 1];
+        int       i, j;
         
-        for (int i = 0; i <= len1; i++) {
-            for (int j = 0; j <= len2; j++){
+        for (i = 0; i <= len1; i++) {
+            for (j = 0; j <= len2; j++){
                 
                 if (i == 0 || j == 0)
                     arr[i][j] = i + j;
